3628ba4b9669dad76b1b55bcb3a44d373a288931
Jeffrey McAteer <jeffrey.p.mcateer@gmail.com>
2020-11-25 14:08:35 -0500

    added archlive files

---
+++ archlive/profiledef.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC2034
+
+iso_name="asure-os-baseline"
+iso_label="ARCH_$(date +%Y%m)"
+iso_publisher="Jeffrey McAteer <jeffrey.p.mcateer@gmail.com>"
+iso_application="Azure OS"
+iso_version="$(date +%Y.%m.%d)"
+install_dir="arch"
+bootmodes=('uefi-x64.systemd-boot.esp' 'uefi-x64.systemd-boot.eltorito')
+arch="x86_64"
+pacman_conf="pacman.conf"
+
--- build.sh
+++ build.sh
@@ -7,8 +7,8 @@ crun() {
     mkdir '.crun'
   fi
   RAN_FILE=".crun/${1}"
-  if ! [ -e RAN_FILE ] ; then
-    ${1} && touch $RAN_FILE
+  if ! [ -e "$RAN_FILE" ] ; then
+    ${1} && touch "$RAN_FILE"
   fi
 }
 
@@ -21,13 +21,43 @@ crun install_deps
 
 # archlive is checked in to source control, so this only runs if you've thrown it out.
 if ! [ -e archlive ] ; then
-  cp -r /usr/share/archiso/configs/profile/baseline archlive
+  cp -r /usr/share/archiso/configs/baseline archlive
 fi
 
 cd archlive
-./build.sh
 
+sudo mkarchiso \
+  -A 'AzureOS' \
+  -L 'AzureOS' \
+  -P 'jeffrey mcateer <jeffrey.p.mcateer@gmail.com>' \
+  -C pacman.conf \
+  -p yay \
+  -v $(pwd)
 
+ISO_IMG=$(find . -name '*.iso' 2>/dev/null | head -n 1 | tr -d '\n')
+echo "DONE! Built $ISO_IMG"
+echo "To clean, run:  sudo rm -rf archlive/out archlive/work"
+
+echo "Booting in qemu..."
+
+AZURE_OS_HDA="/mnt/wdb/azure_os_hda.img"
+if ! [ -e "$AZURE_OS_HDA" ] ; then
+  sudo qemu-img create "$AZURE_OS_HDA" 24G
+  sudo chown jeffrey "$AZURE_OS_HDA"
+fi
+
+# Need `yay -S edk2-ovmf` for /usr/share/edk2-ovmf/x64/OVMF_CODE.fd
+OVMF_CODE="/usr/share/edk2-ovmf/x64/OVMF_CODE.fd"
+if ! [ -e "$OVMF_CODE" ] ; then
+  echo "Please install 'edk2-ovmf' so the file $OVMF_CODE is available."
+fi
+
+qemu-system-x86_64 \
+  -bios "$OVMF_CODE" \
+  -boot d \
+  -cdrom $ISO_IMG \
+  -m 2048 \
+  -drive format=raw,file="$AZURE_OS_HDA"
 
 
 
