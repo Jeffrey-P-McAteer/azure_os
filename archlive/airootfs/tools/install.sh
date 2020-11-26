14c450abbb0b7a545b8a732c7476478d21623cfb
Jeffrey McAteer <jeffrey.p.mcateer@gmail.com>
2020-11-26 11:51:03 -0500

    upgrade to GPT partition table

--- archlive/airootfs/tools/install.sh
+++ archlive/airootfs/tools/install.sh
@@ -69,7 +69,7 @@ sudo swapoff "$INSTALL_DEVICE"* || true
 
 set +e
 sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | fdisk ${INSTALL_DEVICE}
-  o # clear the in memory partition table
+  g # replace the in memory partition table with an empty GPT table
   n # new partition
   p # primary partition
   1 # partition number 1
