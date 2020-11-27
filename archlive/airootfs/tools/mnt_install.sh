d4c18380d8823a6e674e4c88d216559dc17d5d44
Jeffrey McAteer <jeffrey.p.mcateer@gmail.com>
2020-11-27 12:42:46 -0500

    xautolock

--- archlive/airootfs/tools/mnt_install.sh
+++ archlive/airootfs/tools/mnt_install.sh
@@ -158,7 +158,7 @@ sudo -u jeffrey yay -S \
 
 sudo -u jeffrey yay -S \
     --noconfirm --answerdiff=None \
-    radicale qemu edk2-ovmf unclutter
+    radicale qemu edk2-ovmf unclutter xautolock
 
 sudo -u jeffrey yay -S \
     --noconfirm --answerdiff=None \
