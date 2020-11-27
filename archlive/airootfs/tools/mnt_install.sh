bbb7f27c16a356a8567abe02b99f5d9fed272561
Jeffrey McAteer <jeffrey.p.mcateer@gmail.com>
2020-11-27 18:47:55 -0500

    added /etc/systemd/logind.conf

--- archlive/airootfs/tools/mnt_install.sh
+++ archlive/airootfs/tools/mnt_install.sh
@@ -200,6 +200,50 @@ load-module module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymous=1
 
 EOF
 
+cat <<EOF >/etc/systemd/logind.conf
+#  This file is part of systemd.
+#
+#  systemd is free software; you can redistribute it and/or modify it
+#  under the terms of the GNU Lesser General Public License as published by
+#  the Free Software Foundation; either version 2.1 of the License, or
+#  (at your option) any later version.
+#
+# Entries in this file show the compile time defaults.
+# You can change settings by editing this file.
+# Defaults can be restored by simply deleting this file.
+#
+# See logind.conf(5) for details.
+
+[Login]
+#NAutoVTs=6
+#ReserveVT=6
+#KillUserProcesses=no
+#KillOnlyUsers=
+#KillExcludeUsers=root
+#InhibitDelayMaxSec=5
+#HandlePowerKey=poweroff
+#HandleSuspendKey=suspend
+#HandleHibernateKey=hibernate
+#HandleLidSwitch=suspend
+#HandleLidSwitchExternalPower=suspend
+#HandleLidSwitchDocked=ignore
+#PowerKeyIgnoreInhibited=no
+#SuspendKeyIgnoreInhibited=no
+#HibernateKeyIgnoreInhibited=no
+#LidSwitchIgnoreInhibited=yes
+#HoldoffTimeoutSec=30s
+#IdleAction=ignore
+#IdleActionSec=30min
+#RuntimeDirectorySize=10%
+#RuntimeDirectoryInodes=400k
+#RemoveIPC=yes
+#InhibitorsMax=8192
+#SessionsMax=8192
+
+HandleLidSwitch=ignore
+
+EOF
+
 
 sudo -u jeffrey python3 -m pip install --user pyftpdlib
 python3 -m pip install --user pyftpdlib
