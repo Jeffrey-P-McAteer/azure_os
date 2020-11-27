3e2184d62a2ffeacaa271e4466987ebac058e92e
Jeffrey McAteer <jeffrey.p.mcateer@gmail.com>
2020-11-27 12:59:18 -0500

    added rsync

--- archlive/airootfs/tools/mnt_install.sh
+++ archlive/airootfs/tools/mnt_install.sh
@@ -158,7 +158,7 @@ sudo -u jeffrey yay -S \
 
 sudo -u jeffrey yay -S \
     --noconfirm --answerdiff=None \
-    radicale qemu edk2-ovmf unclutter xautolock
+    radicale qemu edk2-ovmf unclutter xautolock rsync
 
 sudo -u jeffrey yay -S \
     --noconfirm --answerdiff=None \
