99757c2e704033f072198c445529e1f7df83bb93
Jeffrey McAteer <jeffrey.p.mcateer@gmail.com>
2023-05-13 12:57:11 -0400

    moved qemu to test script

--- build.sh
+++ build.sh
@@ -196,61 +196,12 @@ sudo mkarchiso \
 
 ISO_IMG=$(find $(pwd) -name '*.iso' 2>/dev/null | head -n 1 | tr -d '\n')
 echo "DONE! Built $ISO_IMG"
-echo "To clean, run:  sudo rm -rf archlive/out archlive/work"
 
-echo "Booting in qemu..."
+cd ..
 
-#AZURE_OS_HDA="/mnt/wdb/azure_os_hda.img"
-#AZURE_OS_HDA="/j/downloads/azure_os_hda.img"
-# # AZURE_OS_HDA="/dev/sda"
-
-AZURE_OS_HDA="/mnt/scratch/azure_os_hda.img"
-
-if grep -q '/dev/' <<<"$AZURE_OS_HDA" ; then
-  cat <<EOF
-WARNING: AZURE_OS_HDA=$AZURE_OS_HDA, which looks like a physical disk!
-Ok to overwrite it?
-EOF
-  read yn
-  if ! grep -qi 'y' in <<<"$yn" ; then
-    echo "Exiting..."
-    exit 1
-  fi
+if ! [ -e test.sh ] ; then
+  cd ..
 fi
 
-if ! [ -e "$AZURE_OS_HDA" ] ; then
-  sudo qemu-img create "$AZURE_OS_HDA" 24G
-  sudo chown jeffrey "$AZURE_OS_HDA"
-fi
-
-
-# Need `yay -S edk2-ovmf` for /usr/share/edk2-ovmf/x64/OVMF_CODE.fd
-OVMF_CODE="/usr/share/edk2-ovmf/x64/OVMF_CODE.fd"
-if ! [ -e "$OVMF_CODE" ] ; then
-  echo "Please install 'edk2-ovmf' so the file $OVMF_CODE is available."
-  exit 1
-fi
-
-cat <<EOF
-
-====================== BOOTING ======================
-
-EOF
-
-qemu-system-x86_64 \
-  -bios "$OVMF_CODE" \
-  -enable-kvm -cpu host -boot d \
-  -cdrom "$ISO_IMG" \
-  -nographic -serial mon:stdio \
-  -m 3048 -net user -net nic \
-  -drive format=raw,file="$AZURE_OS_HDA"
-
-cat <<EOF
-To run a test VM:
-
-qemu-system-x86_64 -bios "$OVMF_CODE" -enable-kvm -cpu host -m 3048 -net user -net nic -drive format=raw,file="$AZURE_OS_HDA"
-
-EOF
-
-
+./test.sh
 
