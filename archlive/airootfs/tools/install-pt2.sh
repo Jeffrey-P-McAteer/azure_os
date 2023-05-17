8eea626f7965802116f47a9b736f3af3d8a82835
Jeffrey McAteer <jeffrey.p.mcateer@gmail.com>
2023-05-17 09:00:42 -0400

    WHY THE F is swaybg a seperate package?!?!?!?!????

--- archlive/airootfs/tools/install-pt2.sh
+++ archlive/airootfs/tools/install-pt2.sh
@@ -154,7 +154,7 @@ jeff_packages=(
   adobe-source-han-sans-jp-fonts adobe-source-han-serif-jp-fonts
   
   ## Wayland DE
-  sway
+  sway swaybg
 
   ## Audio tools
   pipewire pipewire-audio pipewire-alsa pipewire-pulse
